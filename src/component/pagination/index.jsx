53547e50d86b2979eb654230b0ed9b264fc4ae9b
ramyaagowri <ramyaagowrik@gmail.com>
2023-09-27 09:41:48 +0530

    after pagination

--- src/component/pagination/index.jsx
+++ src/component/pagination/index.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import "./style.css";
 
 const Pagination = ({ total, perpage, currentPage, setCurrentPage }) => {
-    console.log(currentPage)
     const pages = [];
-    console.log("From pagination")
+
     for (let i = 1; i <= Math.ceil(total / perpage); i++) {
         pages.push(i);
     }
 
     const handlePrevClick = () => {
         if (currentPage > 1) {
-            console.log("Called prev")
             setCurrentPage(currentPage - 1);
         }
     };
@@ -22,22 +20,34 @@ const Pagination = ({ total, perpage, currentPage, setCurrentPage }) => {
         }
     };
 
+    const pageRange = 4;
+    const startIndex = Math.max(currentPage - Math.floor(pageRange / 2), 1);
+    const endIndex = Math.min(startIndex + pageRange - 1, pages.length);
+
     return (
         <div className="pageContainer">
-            <button onClick={() => handlePrevClick()} className={currentPage - 1 ? "button" : "disablebutton"}>Prev</button>
-            {pages.map((page) => (
+            <button
+                onClick={() => handlePrevClick()}
+                className={currentPage > 1 ? "button" : "disablebutton"}
+            >
+                Prev
+            </button>
+            {pages.slice(startIndex - 1, endIndex).map((page) => (
                 <button
                     key={page}
                     onClick={() => setCurrentPage(page)}
                     className={currentPage === page ? "pageactive" : "button"}
                 >
                     {page}
-                    {console.log(typeof page)}
-
                 </button>
             ))}
-            <button onClick={() => handleNextClick()} className={currentPage === pages.length ? "disablebutton" : "button"}>Next</button>
-        </div >
+            <button
+                onClick={() => handleNextClick()}
+                className={currentPage < pages.length ? "button" : "disablebutton"}
+            >
+                Next
+            </button>
+        </div>
     );
 };
 
